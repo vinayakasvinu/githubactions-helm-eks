a74ecd7149c72a920b3f0bd4bea121f15b0e020f
bkrraj <bkrraj@gmail.com>
2023-07-13 15:02:12 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -5,11 +5,11 @@ terraform {
     key            = "terraform-eks-asset.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    hash_key     = "Lock_Id"
+    hash_key     = "LockId"
   
 
   attribute {
-    name = "Lock_Id"
+    name = "LockId"
     type = "S"
   }
 }
